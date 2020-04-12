e29d690bb7554be647f6809d664ccfb5d92c6835
Eton <pk2p007@gmail.com>
2020-04-13 00:05:09 +0800

    儲存點擊的項目

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,15 @@ class RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : Colors.green,
       ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
